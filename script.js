614b3a4e8be9c366cd29f9ce93aa1a1316375e59
Lawrence <146691376+cedik456@users.noreply.github.com>
2024-07-05 11:03:32 +0800

    Learn How to Add & Change Html Elements
    Using DOM (Document Object Model)

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ let p2 = "David";
 let gender = "she";
 let drinks = "coffee"; */
 
+// const { even } = require("prelude-ls");
+
+// const { even } = require("prelude-ls");
+
+// const { even } = require("prelude-ls");
+
 // import { yellow } from "color-name";
 
 // import { create } from "file-entry-cache";
@@ -1796,6 +1802,9 @@ for(let people of names) {
 //   ulElement.style.listStyle = "none";
 // });
 
+// const foods = document.querySelector("li");
+// const fruits = document.querySelector(".fruits");
+
 // const foods = document.querySelectorAll("li");
 // const fruits = document.querySelectorAll(".fruits");
 
@@ -1809,6 +1818,134 @@ for(let people of names) {
 
 // fruits[2].style.backgroundColor = "yellow";
 // foods[5].style.backgroundColor = "green";
+
+// foods.style.backgroundColor = "yellow";
+// fruits.style.backgroundColor = "green";
+
+// DOM NAVIGATION
+
+// first element child *********************
+
+// const ulElements = document.querySelectorAll("ul");
+
+// ulElements.forEach(ulElement => {
+//   const firstChild = ulElement.firstElementChild;
+//   firstChild.style.backgroundColor = "yellow";
+// });
+
+// last element child ******************
+
+// const element = document.getElementById("dessert");
+// const lastElement = element.lastElementChild;
+
+// lastElement.style.backgroundColor = "yellow";
+
+// const elements = document.querySelectorAll("ul");
+
+
+// elements.forEach(element => {
+  //   const lastElement = element.lastElementChild;
+  //   lastElement.style.backgroundColor = "yellow";
+  // });
+
+  // next element sibling *************
+
+  // const element = document.getElementById("cake");
+  // const nextElement = element.nextElementSibling;
+
+  // nextElement.style.backgroundColor = "yellow"; 
+
+  // const element = document.getElementById("fruits");
+
+  // const nextSibling = element.nextElementSibling;
+
+  // nextSibling.style.backgroundColor = "pink";
+
+  // previous element sibling **********
+
+  // const element = document.getElementById("dessert");
+  // const prevSibling = element.previousElementSibling;
+
+  // prevSibling.style.backgroundColor = "green";
+
+  // parent element
+
+  // const element = document.getElementById("lettuce");
+  // const parent = element.parentElement;
+
+  // parent.style.backgroundColor = "pink";
+
+  // children element
+
+  // const element = document.getElementById("fruits");
+  // const children = element.children;
+
+  // children[1].style.backgroundColor = "yellow";
+
+
+  // ADD & CHANGE HTML (USING DOM)
+
+  // step 1. Create the element ***
+
+  // const newListItem = document.createElement("li");
+
+  // step 2. Add attributes/properties ***
+
+  // newListItem.textContent = "coconut";
+  // newListItem.id = "coconut";
+  // newListItem.style.alignItems = "center";
+  // newListItem.style.backgroundColor = "brown";
+
+  // step 3. Append element to the DOM ***
+
+  // document.body.append(newListItem);
+  // document.body.prepend(newListItem);
+  // document.getElementById("fruits").append(newListItem);
+  // document.getElementById("fruits").prepend(newListItem);
+
+  // const listItems = document.querySelectorAll("#fruits li");
+  // document.getElementById("fruits").insertBefore(newListItem, listItems[4]);
+
+  // const orange = document.getElementById("orange");
+  // document.getElementById("fruits").insertBefore(newListItem, orange);
+
+  // const box2 = document.getElementById("box2");
+  // document.body.insertBefore(newListItem, box2)
+
+  // const boxes = document.querySelectorAll(".box");
+  // document.body.insertBefore(newListItem, boxes[2]);
+
+  // step 4. Remove html element ***
+
+  // document.body.removeChild(newListItem);
+// document.getElementById("fruits").removeChild(newListItem);
+
+// document.getElementById("box4").append(newListItem);
+// document.getElementById("box4").removeChild(newListItem);
+
+// ADD EVENT LISTENERS ( .addEventListener(event,callback or arrow function()) )
+
+const myBox = document.getElementById("myBox");
+const myButton = document.getElementById("myButton");
+
+myButton.addEventListener("click", event => {
+  myBox.style.backgroundColor = "tomato";
+  myBox.textContent = "You just clicked!";
+});
+
+myButton.addEventListener("mouseover", event => {
+  myBox.style.backgroundColor = "pink";
+  myBox.textContent = "Dont do it!!";
+});
+
+myButton.addEventListener("mouseout", event => {
+  myBox.style.backgroundColor = "#3498db";
+  myBox.textContent = "Click Me!";
+});
+
+
+
+
 
 
 

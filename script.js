54f89df34a71159f7148f83cfe6962d454d80b55
Lawrence <146691376+cedik456@users.noreply.github.com>
2024-07-13 18:15:07 +0800

    Learned how to fetch JSON files and HTTP requests

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ let drinks = "coffee"; */
 
 // const { values } = require("prelude-ls");
 
+// const { values } = require("prelude-ls");
+
 // const { reject, values } = require("prelude-ls");
 // const { resolve } = require("uri-js");
 
@@ -2513,7 +2515,58 @@ for(let people of names) {
 
 // FETCH ---------------------------------------------------
 
-fetch("names.json").then(response => response.json()).then(values => console.log(values))
+// 1. Fetch json files
+
+// fetch("person.json")
+//     .then(response => response.json())
+//     .then(values => values.for)
+//     .catch(error => console.error(error))
+
+// 2. Fetch http requests
+
+// fetch("https://pokeapi.co/api/v2/pokemon/pikachu",)
+//     .then(response => {
+
+//         if(!response.ok){
+//           console.log("Unable to fetch the resource")
+//         }
+//         return response.json();
+//     })
+//     .then(data => console.log(data.species))
+//     .catch(error => console.error(error));
+
+// 3. Fetch using async and await
+
+async function fetchData(){
+
+    try{
+
+      const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+      if(!response.ok){
+        throw new Error("There's no pokemon with that name!");
+      }
+
+      const data = await response.json();
+
+      if(!data.sprites || !data.sprites.front_default){
+        throw new Error("There's no pokemon with that name!")
+      }
+      
+      const pokemonSprite = data.sprites.front_default;
+      const pokemonImg = document.getElementById("pokemonSprite");
+
+      pokemonImg.src = pokemonSprite;
+      pokemonImg.style.display = "block";
+
+      document.getElementById("errorMessage").textContent = "";
+    }
+    catch(error){
+      document.getElementById("errorMessage").textContent = error.message;
+
+    }
+}
 
 
 
